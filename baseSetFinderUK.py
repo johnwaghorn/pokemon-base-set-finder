7a8a8669300d9fbac09f372a1778efda5dc38da4
johnwaghorn <john.waghorn1@gmail.com>
2019-11-07 21:03:45 +0000

    cleaning output

--- baseSetFinderUK.py
+++ baseSetFinderUK.py
@@ -5,6 +5,7 @@ import sys
 import os
 import random
 import re
+from string import printable
 
 #colours just in case for terminal
 RED   = "\033[1;31m"  
@@ -41,6 +42,10 @@ def format(soup):
 	print "Formating Data..."
 	results = []
 	cardTitles = []
+	cardTitles2 = []
+	cardPrices = []
+	cardPrices2 = []
+	cardStock = []
 
 
 	#All results in one array - messy
@@ -48,39 +53,71 @@ def format(soup):
 		if len(div.text)>0:
 			results.append(div.text)
 
-	#Put all titles in array, ready for printing later on
+	#------------TITLES-------------------------------------------------------
+	#Put all titles in an array, ready for printing later on
 	for a in soup.find_all('a', attrs={'class':'product-title'}):
 		if len(a.text)>0:
 			cardTitles.append(a.text)
+	#clean up the title array
+	for i in range(len(cardTitles)):
+		currentString = cardTitles[i]
+		currentString = currentString.encode('ascii', 'ignore')
+		cardTitles2.append(currentString)
+		print currentString
+	print len(cardTitles2)
 
-	cardTitles = "\n".join(cardTitles)
-	print cardTitles
+
+	#------------PRICES-------------------------------------------------------
+	for div in soup.find_all('div', attrs={'class': 'ty-product-list__price'}):
+		if len(div.text)>0:
+			cardPrices.append(div.text)
+	#clean up the prices array
+	for i in range(len(cardPrices)):
+		currentString = cardPrices[i]
+		currentString = currentString.encode('ascii', 'ignore')
+		cardPrices2.append(currentString)
+		print currentString
+	print len(cardPrices2)
 
 
+	#cardPrices = "\n".join(cardPrices)
+	
 
-
+	#----------STOCK-----------------------------------------------------------
 	for i in range(len(results)):
 		currentString = results[i]
-
+		currentString = currentString.encode('ascii', 'ignore')
 		#cardTitle = soup.find('a', attrs={'class':'product-title'})
 		#cardTitle = cardTitle.text
-
 		for i in range(len(currentString)):
+			
+			currentString = currentString.rstrip()
+		
+
 			if currentString[i:i+12]=="Out of stock":
 				#results.remove(results[i])
 				sys.stdout.write(RED)
 				
+				cardStock.append('out of stock')
 				print ' @@@@@@@@ OUT OF STOCK :('
-				sys.stdout.write(GREEN)
+				#sys.stdout.write(GREEN)
 				#print cardTitle
-				sys.stdout.write(CYAN)
-				print currentString
+				sys.stdout.write(RED)
+				currentString = currentString.rstrip()
+				#print currentString
+
+			if currentString[i:i+11]=="Add to cart":
+				sys.stdout.write(GREEN)
+				cardStock.append('out of stock')
+				print 'IN STOCK!!! GO BUY HERE:'
 				
+				#currentString = re.sub("[^{}]+".format(printable), "", currentString)
+				#print currentString
 			#Out of stock
 
 
 
-
+	print len(cardStock)
 
 	#results = "\n".join(results)
 	#print results
